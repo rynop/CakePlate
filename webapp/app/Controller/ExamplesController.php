bbcb58fb55baeadd0031112a607c776345f53a4c
rynop <ryan.pendergast@gamail.com>
2011-11-26 00:19:09 -0600

    oops

--- webapp/app/Controller/ExamplesController.php
+++ webapp/app/Controller/ExamplesController.php
@@ -5,7 +5,7 @@ class ExamplesController extends AppController {
 
    function index(){   		
 	   	$this->set('title_for_layout', 'Example');	
-	   	$this->Session->setFlash('There was an error saving the data. please contact support (support@leaguelogix.com)','flash_error',array(),'error');
+	   	$this->Session->setFlash('There was an error saving the data. please contact support','flash_error',array(),'error');
 	   	$this->Session->setFlash('Logo successufully added','flash_success',array(),'success');
    }
 }
